c6febdabcbdff5762764f5ec460cca1d7d4814b8
ninjamike1211 <vandemarkm@gmail.com>
2023-11-07 22:30:33 -0500

    Changes to item glow effect

--- shaders/lib/voxel.glsl
+++ shaders/lib/voxel.glsl
@@ -6,6 +6,7 @@ const int xzRadiusBlocks = voxelMapResolution / 32;
 #define Floodfill_Instant
 // #define Floodfill_Particles
 // #define Floodfill_HeltItemLight
+#define item_glow 0.1 // [0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0]
 
 /*
 #ifdef Floodfill_HeltItemLight
